e2646383060e961913f01044821d0f14a651ce6d
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-09-04 00:13:40 -0700

    Finished Video Section

---
+++ Glozzom/src/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".video").click(function(){
+        var theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCauto = videoSRC + 
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html15-1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function(){
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
