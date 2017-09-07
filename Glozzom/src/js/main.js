79fdab7338f2ef7470673b0f8e3629d5df2ad7d8
Brandon Smith <brandon@reddirtwebdesign.com>
2017-09-07 06:26:06 -0400

    icon boxes and testimonials

--- Glozzom/src/js/main.js
+++ Glozzom/src/js/main.js
@@ -16,3 +16,12 @@ $(document).on('click','[data-toggle="lightbox"]',function(){
     event.preventDefault();
     $(this).ekkoLightbox();
 })
+
+$('.slider').slick({
+  infinite: true,
+  slideToShow: 1,
+  slideToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false
+});
